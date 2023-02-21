109449d55f8c3ce0b21930bce807a22aa9a8cf07
Mmegmaths76 <125988351+Mmegmaths76@users.noreply.github.com>
2023-02-21 14:59:22 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Tortoise)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
